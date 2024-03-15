5ce78c961e92c76280c7605252c2856b2d5bcc3e
sanalSaraswat <saraswatsanal@gmail.com>
2024-03-15 23:26:45 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import { Box } from "@mui/material";
-import Dashboard from "./Dashboard";
 
 function App() {
   return (
 
-    <Routes>
-
-      <Route path="/" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-
-
-    </Routes>
+    <Register />
 
 
   );
